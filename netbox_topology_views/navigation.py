acd60c7175e494a8fe9d618fae8508c0ff208105
mattijs vanhaverbeke <mattijs-v@live.be>
2020-04-16 14:56:11 +0200

    register url

---
+++ netbox_topology_views/navigation.py
@@ -0,0 +1,10 @@
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_topology_views:home',
+        link_text='Topology',
+        buttons=()
+    ),
+)
