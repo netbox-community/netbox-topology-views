b792128415e619acae98262c2509f91bac485be7
mattieserver <3049868+mattieserver@users.noreply.github.com>
2022-03-10 11:46:00 +0100

    Fix dark mode (#90)
    * fixed dark mode

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netbox-topology-views',
-    version='1.0.1',
+    version='1.0.2',
     description='An NetBox plugin to create Topology maps',
     url='https://github.com/mattieserver/netbox-topology-views',
     author='Mattijs Vanhaverbeke',
