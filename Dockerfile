ce39c9839691cb2da099ed099a8d981bad9ec04a
Mattijs Vanhaverbeke <mattijs.vanhaverbeke@ebo-enterprises.com>
2022-08-03 12:07:58 +0200

    bump version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM netboxcommunity/netbox:latest
 
-COPY ./plugin_requirements.txt /
+COPY ./docker/plugin_requirements.txt /
 RUN /opt/netbox/venv/bin/pip install  --no-warn-script-location -r /plugin_requirements.txt
--- netbox_topology_views/__init__.py
+++ netbox_topology_views/__init__.py
@@ -4,7 +4,7 @@ class TopologyViewsConfig(PluginConfig):
     name = 'netbox_topology_views'
     verbose_name = 'Topology views'
     description = 'An plugin to render topology maps'
-    version = '2.0.1'
+    version = '2.0.2'
     author = 'Mattijs Vanhaverbeke'
     author_email = 'author@example.com'
     base_url = 'netbox_topology_views'
