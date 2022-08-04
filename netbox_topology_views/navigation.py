a9e3862aba87a374defa290debe6dcf455074817
Andrea Dainese <andrea.dainese@pm.me>
2022-08-04 12:56:47 +0000

    implement L3 topology

--- netbox_topology_views/navigation.py
+++ netbox_topology_views/navigation.py
@@ -3,7 +3,11 @@ from utilities.choices import ButtonColorChoices
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_topology_views:home',
-        link_text='Topology'
+        link='plugins:netbox_topology_views:l2',
+        link_text='L2 Topology'
+    ),
+    PluginMenuItem(
+        link='plugins:netbox_topology_views:l3',
+        link_text='L3 Topology'
     ),
 )
