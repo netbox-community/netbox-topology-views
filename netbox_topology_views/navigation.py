de2724aed1817354ae853d7925065442684f311f
mattieserver <3049868+mattieserver@users.noreply.github.com>
2022-12-13 16:55:56 +0100

    new layout

--- netbox_topology_views/navigation.py
+++ netbox_topology_views/navigation.py
@@ -1,8 +1,14 @@
 from extras.plugins import PluginMenuItem
 
-menu_items = (
-    PluginMenuItem(link="plugins:netbox_topology_views:home", link_text="Topology"),
-    PluginMenuItem(
-        link="plugins:netbox_topology_views:images", link_text="Topology Images"
+menu = PluginMenu(
+    label='Topology Views',
+    groups=(
+        ('Topology', ( PluginMenuItem(link="plugins:netbox_topology_views:home", link_text="Topology"), PluginMenuItem(link="plugins:netbox_topology_views:images", link_text="Topology Images"), )),
     ),
 )
+
+#menu_items = (
+#    PluginMenuItem(link="plugins:netbox_topology_views:home", link_text="Topology"),
+#    PluginMenuItem(link="plugins:netbox_topology_views:images", link_text="Topology Images"),
+#    ),
+#)
