fd801924d800db47754ca21a037ad78035d020c6
Mattijs Vanhaverbeke <mattijs.vanhaverbeke@ebo-enterprises.com>
2022-12-18 18:49:33 +0100

    bump version

--- netbox_topology_views/__init__.py
+++ netbox_topology_views/__init__.py
@@ -5,7 +5,7 @@ class TopologyViewsConfig(PluginConfig):
     name = "netbox_topology_views"
     verbose_name = "Topology views"
     description = "An plugin to render topology maps"
-    version = "3.1.0"
+    version = "3.1.1"
     author = "Mattijs Vanhaverbeke"
     author_email = "author@example.com"
     base_url = "netbox_topology_views"
