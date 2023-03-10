c4ee38c0c9bc8aa92a31cba9d3f39be7f611b570
Jan Krupa <JanKrupa93@gmail.com>
2023-03-10 09:29:33 +0100

    Change menu navigation icon (#266)

--- netbox_topology_views/navigation.py
+++ netbox_topology_views/navigation.py
@@ -2,6 +2,7 @@ from extras.plugins import PluginMenu, PluginMenuItem
 
 menu = PluginMenu(
     label='Topology Views',
+    icon_class="mdi mdi-sitemap",
     groups=(
         ('Topology', (PluginMenuItem(link="plugins:netbox_topology_views:home", link_text="Topology"),),),
         ('CUSTOMIZATION', (PluginMenuItem(link="plugins:netbox_topology_views:images", link_text="Topology Images"),),),
