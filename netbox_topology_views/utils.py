cf8a01c21351b8a4d6d759d6c98ec7c48a50d2ed
Daniel Bremer <daniel.bremer@mpsd.mpg.de>
2024-04-29 14:21:23 +0200

    Add option to disable gravity and smooth cables

--- netbox_topology_views/utils.py
+++ netbox_topology_views/utils.py
@@ -177,8 +177,12 @@ def get_query_settings(request):
     if "show_neighbors" in request.GET:
         if request.GET["show_neighbors"] == "True" :
             show_neighbors = True
-    
-    return filter_id, save_coords, show_unconnected, show_power, show_circuit, show_logical_connections, show_single_cable_logical_conns, show_cables, show_wireless, group_sites, group_locations, group_racks, show_neighbors
+
+    disable_curved_cables = settings.PLUGINS_CONFIG["netbox_topology_views"]["disable_curved_cables"]
+
+    disable_gravity = settings.PLUGINS_CONFIG["netbox_topology_views"]["disable_gravity"]
+
+    return filter_id, save_coords, show_unconnected, show_power, show_circuit, show_logical_connections, show_single_cable_logical_conns, show_cables, show_wireless, group_sites, group_locations, group_racks, show_neighbors, disable_curved_cables, disable_gravity
 
 class LinePattern():
     wireless = [2, 10, 2, 10]
