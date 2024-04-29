0c44197dcbbb07bb4ec3f7002b2dc74f6b4fda47
Mario <github@franzbonenkamp.de>
2024-04-29 20:25:21 +0200

    Change extras.plugins to netbox.plugins (#487)

--- netbox_topology_views/__init__.py
+++ netbox_topology_views/__init__.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginConfig
+from netbox.plugins import PluginConfig
 
 
 class TopologyViewsConfig(PluginConfig):
--- netbox_topology_views/template_content.py
+++ netbox_topology_views/template_content.py
@@ -1,5 +1,5 @@
 
-from extras.plugins import PluginTemplateExtension
+from netbox.plugins import PluginTemplateExtension
 from django.conf import settings
 from packaging import version
 
