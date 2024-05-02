2cb5c286af56c7e3ec9bfc142d5a79fa5b3e1445
Daniel Bremer <daniel.bremer@mpsd.mpg.de>
2024-05-02 09:46:00 +0200

    moved options to settings

--- netbox_topology_views/utils.py
+++ netbox_topology_views/utils.py
@@ -178,11 +178,7 @@ def get_query_settings(request):
         if request.GET["show_neighbors"] == "True" :
             show_neighbors = True
 
-    disable_smooth_cables = ""
-
-    disable_physics = ""
-
-    return filter_id, save_coords, show_unconnected, show_power, show_circuit, show_logical_connections, show_single_cable_logical_conns, show_cables, show_wireless, group_sites, group_locations, group_racks, show_neighbors, disable_smooth_cables, disable_physics
+    return filter_id, save_coords, show_unconnected, show_power, show_circuit, show_logical_connections, show_single_cable_logical_conns, show_cables, show_wireless, group_sites, group_locations, group_racks, show_neighbors
 
 class LinePattern():
     wireless = [2, 10, 2, 10]
