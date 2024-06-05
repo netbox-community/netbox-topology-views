cfabf1486a1d78feedb771bd435331ce9ae49b8b
mvanhaverbeke <mattijs.vanhaverbeke@ebo-enterprises.com>
2024-06-05 13:23:07 +0200

    4.0.0-beta.1 release

--- netbox_topology_views/__init__.py
+++ netbox_topology_views/__init__.py
@@ -5,7 +5,7 @@ class TopologyViewsConfig(PluginConfig):
     name = "netbox_topology_views"
     verbose_name = "Topology views"
     description = "An plugin to render topology maps"
-    version = "3.9.1"
+    version = "4.0.0-beta.1"
     author = "Mattijs Vanhaverbeke"
     author_email = "author@example.com"
     base_url = "netbox_topology_views"
