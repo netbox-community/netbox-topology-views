f8c6e9e193ebe2f9352632283a0790e46334bbfd
mattieserver <3049868+mattieserver@users.noreply.github.com>
2024-08-26 12:21:00 +0200

    bump version (#558)

--- netbox_topology_views/__init__.py
+++ netbox_topology_views/__init__.py
@@ -5,7 +5,7 @@ class TopologyViewsConfig(PluginConfig):
     name = "netbox_topology_views"
     verbose_name = "Topology views"
     description = "An plugin to render topology maps"
-    version = "4.0.0"
+    version = "4.0.1"
     author = "Mattijs Vanhaverbeke"
     author_email = "author@example.com"
     base_url = "netbox_topology_views"
