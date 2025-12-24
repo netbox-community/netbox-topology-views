8806293598a2479743d57e471fddd7f0a4a2bb66
ray34g <Go.Ray@ray34g.com>
2025-12-24 21:57:50 +0900

    Fix crash when rendering WirelessLink without label

--- netbox_topology_views/views.py
+++ netbox_topology_views/views.py
@@ -322,10 +322,9 @@ def create_edge(
         edge["color"] = '#f1c232'
         edge["href"] = interface.get_absolute_url() + "trace"
     
-    if cable is not None and cable.label:
-        cable_label = "<br>Label: " + cable.label
-    else:
-        cable_label = ""
+    cable_label = ""
+    if cable is not None and hasattr(cable, "label") and cable.label:
+        cable_label = "<br>Label: " + str(cable.label)
 
     edge[
         "title"
